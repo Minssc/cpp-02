9b48f863ae1bf68878bcb73a73f0f487d71fb339
minsunki <minsunki@student.42seoul.kr>
2022-03-28 12:48:05 +0900

    ex01 touchup

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: minsunki <minsunki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 18:18:21 by minsunki          #+#    #+#             */
-/*   Updated: 2022/03/25 21:08:10 by minsunki         ###   ########seoul.kr  */
+/*   Updated: 2022/03/28 12:47:09 by minsunki         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ Fixed::Fixed(void): _raw(0)
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(int a)
+Fixed::Fixed(const int a)
 {
 	std::cout << "Int constructor called" << std::endl;
 	_raw = a << _n_bits;
 }
 
-Fixed::Fixed(float a)
+Fixed::Fixed(const float a)
 {
 	std::cout << "Float constructor called" << std::endl;
 	_raw = roundf(a * (1 << _n_bits));
